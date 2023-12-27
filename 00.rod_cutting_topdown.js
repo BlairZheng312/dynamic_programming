a66c347f429f7c075e6e6f4ac133a0a86ca9e0a0
blair <blairzheng0312@gmail.com>
2023-12-27 11:53:25 +1100

    rod_cutting_topdown

---
+++ 00.rod_cutting_topdown.js
@@ -0,0 +1,21 @@
+// dynamic programming: to divide problem into smaller sub set
+// each sub set reach the optimal result => the final solution will be optimal
+
+// rod_cutting => to cut the rod in a way to meet the total length where the profits are the most
+// top_down method: recursive => the sub set will be re-calculated
+
+let priceList = [0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
+
+function rodCuttingTopDown(priceList, length) {
+    if (length === 0) {
+        return 0
+    } else {
+        let bestCut = priceList[length]
+        for (let i = 1; i < length; i++) {
+            bestCut = Math.max(bestCut, priceList[i] + rodCuttingTopDown(priceList, length - i))
+        }
+        return bestCut
+    }
+}
+
+console.log(rodCuttingTopDown(priceList, 9))
