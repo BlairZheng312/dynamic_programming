318d3be7acbea897f680a7f1220acad19ea72b32
blair <blairzheng0312@gmail.com>
2023-12-29 20:45:49 +1100

    rod_cutting_reconstruction

--- 01.rod_cutting_bottomup.js
+++ 01.rod_cutting_bottomup.js
@@ -3,23 +3,51 @@
 // non-recursive => the sub set will be calculated only once
 // time_complexity: n**2
 
-let priceList = [0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
+let priceList = [0, 1, 5, 8, 9, 10, 17, 17, 20, 21, 23, 24, 26, 27, 27, 28, 30, 33, 36, 39, 40]
 
 function rodCuttingBottomUp(priceList, length) {
-    let bestCuts = []
+    // to store the highest profts of given length
+    let mostProfits = []
+
+    // to store the left cut to reach the highest profit of given length
+    // to trace back and restore the cuts of the highest profit
+    let leftCuts = []
+
     if (length === 0) {
-        bestCuts.push(priceList[0])
+        mostProfits.push(priceList[0])
+        cutLefts.push(0)
     } else {
-        bestCuts.push(...priceList.slice(0, 2))
-        for (let i = 2; i <= length; i++) {
-            let bestCut = priceList[i]
+        mostProfits.push(priceList[0])
+        leftCuts.push(0)
+        for (let i = 1; i <= length; i++) {
+            let mostProfit = priceList[i]
+            let leftCut = i
             for (let j = 1; j < i; j++) {
-                bestCut = Math.max(bestCut, priceList[j] + bestCuts[i - j])
+                if (priceList[j] + mostProfits[i - j] > mostProfit) {
+                    mostProfit = priceList[j] + mostProfits[i - j]
+                    leftCut = j
+                }
             }
-            bestCuts.push(bestCut)
+            mostProfits.push(mostProfit)
+            leftCuts.push(leftCut)
         }
     }
-    return bestCuts[length]
+
+    // to store the cuts to reach the highest profit of given length
+    // restore the cuts by tracing back the left cuts of each given length
+    let cuts = []
+    let subLength = length
+    while (leftCuts[subLength] > 0) {
+        cuts.push(leftCuts[subLength])
+        subLength = subLength - leftCuts[subLength]
+    }
+
+    return {
+        profits: mostProfits[length],
+        cuts
+    }
 }
 
-console.log(rodCuttingBottomUp(priceList, 9))
+const { profits, cuts } = rodCuttingBottomUp(priceList, 20)
+console.log(profits)
+console.log(cuts)
